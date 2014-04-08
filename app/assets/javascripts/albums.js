f6da9fd579cdcf7083e44f8bc4852cfd6e79b6f2
Michael Todd <mikejtodd@gmail.com>
2014-04-08 12:08:07 -0500

    Now with pushState

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   
-  $(".album").on("click", function(event){
+  $(".album").on("click", function(){
+    
     $(".album_details").empty();
     $(".album_details").hide();
     $(".album").removeClass("to_top");
@@ -8,10 +9,19 @@ $(document).ready(function(){
     $(".album:nth-child(2)").animate({left: '-349px',}, 700 );
     $(".album:nth-child(3)").animate({left: '-698px',}, 700 );
     $("#albums").addClass("albums_left");
-    event.preventDefault();
+    
+  });
+  
+  $(window).bind('popstate', function() {
+      $.ajax({url:location.pathname+'?rel=album',success: function(data){
+          $('#content').html(data);
+      }});
   });
   
   $("#week").on("click", function(event){
+    
+    pageurl = $(this).children().attr('href');
+    
     $(".album:nth-child(2)").animate({left: '0px',}, 700 );
     $(".album:nth-child(3)").animate({left: '0px',}, 700 );
     setTimeout(function(){
@@ -19,6 +29,12 @@ $(document).ready(function(){
     $( ".album_details" ).fadeOut( "slow" );
     setTimeout(function(){
       $(".album_details").remove();}, 700);
+      
+      if(pageurl!=window.location){
+          window.history.pushState({path:pageurl},'<%= @title %>',pageurl);
+      }
+    
+      return false;
   })
   
 });
--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,16 +1,24 @@
 class LoginsController < ApplicationController 
   def new
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
   def create
     writer = Writer.find_by_email(params[:email])
     
-    if writer && writer.authenticate(params[:password])
-      session[:writer_id] = writer.id
-      redirect_to :home
-    else
-      raise "Login failed. Try again."
+    respond_to do |format|
+      format.html { redirect_to :root }
+      format.js
+      if writer && writer.authenticate(params[:password])
+        session[:writer_id] = writer.id
+      else
+        raise "Login failed. Try again."
+      end
     end
+  
   end
   
   def destroy
