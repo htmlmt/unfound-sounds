48cc97bda52f61c11b97ecfcc97edb991eabaf63
Michael Todd <mikejtodd@gmail.com>
2014-04-08 13:24:08 -0500

    Edited application.rb file

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,10 @@ module UnfoundSounds
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    config.assets.initialize_on_precompile = false
+    
+    config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
