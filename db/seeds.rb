c4f70ca564d9dfd13a4c97da007510436c480260
Michael Todd <mikejtodd@gmail.com>
2014-04-10 15:35:46 -0500

    Now with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(album_title: "'Young Man in America' by Anais Mitchell", album_cover: "http://img02.cdn2-rdio.com/album/2/5/2/000000000014e252/square-600.jpg", album_review: "These songs weave rich tales with characters far removed from 2012. The language Anais Mitchell sculpts could sound stilted coming out of a more mellifluous voice. Hers boasts an aged quality in 31-year-old vocal cords, sharp but beautiful.", rdio: "https://rd.io/i/QUWZfCJLsBM/", week: 2, week_name: "April 14-20", hidden_place: "somewhere in Happy Hollow", review_link: "http://www.hearnebraska.org/content/michael-todds-2012-list#albumsnationally")
