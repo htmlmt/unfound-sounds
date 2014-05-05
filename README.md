2b1bbc4b74bbd93c89a1dbefe3711a8bb702a075
Michael Todd <mikejtodd@gmail.com>
2014-05-05 16:59:35 -0500

    Added readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-unfound-sounds
-==============
+# Where the vinyl is free, if you know where to look
+Unfound Sounds is a site that facilitates record treasure hunts. Through a clean, responsive design, this Rails app succeeds in presenting a fairly simple backend via a thought-out and intuitive design.
+
+## CSS & JavaScript
+
+The bulk of this project's magic lies in client-side technologies. Using from-scratch CSS and JavaScript, the site takes the user through a smooth, AJAX-ified experience, revealing views for both unfound and found albums, and allowing visitors to navigate through past weeks without having to refresh the page.
+
+## Planned features
+
+Next up on the agenda is adding functionality to allow users to provide their location through their phone or desktop browser, with which the site will calculate distances to album locations. 
+
+Please email me at [hello@michaelsedits.com](hello@michaelsedits.com) if you'd like to contribute ideas, and feel free to fork the site. I welcome all pull requests.
--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -49,6 +49,45 @@ $(document).ready(function(){
     event.preventDefault();
   });
   
+  $("#members_only").on("click", function(){
+    $("#member_big").show();
+    $("#member_side").show();
+  })
+
+  $("#monday_thumbnail").on("click", function(){
+    $("#monday_big").show();
+    $("#monday_side").show();
+  })
+
+  $("#tuesday_thumbnail").on("click", function(){
+    $("#tuesday_big").show();
+    $("#tuesday_side").show();
+  })
+
+  $("#tuesday_thumbnail").on("click", function(){
+    $("#tuesday_big").show();
+    $("#tuesday_side").show();
+  })
+
+  $("#wednesday_thumbnail").on("click", function(){
+    $("#wednesday_big").show();
+    $("#wednesday_side").show();
+  })
+
+  $("#thursday_thumbnail").on("click", function(){
+    $("#thursday_big").show();
+    $("#thursday_side").show();
+  })
+
+  $("#friday_thumbnail").on("click", function(){
+    $("#friday_big").show();
+    $("#friday_side").show();
+  })
+  
+  $(".x_white").on("click", function(){
+    $(".big_hint").hide();
+  })
+  
   $("*").dblclick(function(event){
     event.preventDefault();
   });
