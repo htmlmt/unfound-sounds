2114212a857277868d995df89dae8dafb36226d4
Michael Todd <mikejtodd@gmail.com>
2014-08-03 18:00:30 -0500

    Check for HN review link; updated for Wednesday opening

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module UnfoundSounds
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    config.time_zone = 'Central Time (US & Canada)'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
